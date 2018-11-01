f4cc8e6a2e25e6a6f824c530439ca1a3bbcce945
NPerederiy <nick.perederiy@gmail.com>
2018-11-01 18:58:32 +0200

    Created the app structure and added authentication page
    - Created a WFP project
    - Added new folders
    - Created and added project logo
    - Created and added user icon
    - Added authentication form
    - Added window control buttons and functionality for them

---
+++ eDiary/View/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace eDiary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OnWindowDrag(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        public void OnWindowMinimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        public void OnWindowResize(object sender, RoutedEventArgs e)
+        {
+            switch (WindowState)
+            {
+                case WindowState.Maximized:
+                    WindowState = WindowState.Normal;
+                    break;
+                case WindowState.Normal:
+                    WindowState = WindowState.Maximized;
+                    break;
+            }
+        }
+
+        public void OnWindowClose(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
