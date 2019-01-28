f61a109600313ebae8234da202f3c1049449c261
NPerederiy <nick.perederiy@gmail.com>
2019-01-28 18:06:03 +0200

    Improved display layout of Calendar page. Added day workload
    - Fixed an issue of displaying day-info cards in the month view
    - Added new icons
    - Added Workload enum

--- eDiary.WebClient/src/shared/models/day-info.model.ts
+++ eDiary.WebClient/src/shared/models/day-info.model.ts
@@ -1,11 +1,20 @@
+import { Workload } from "./workload.enum";
+
 export class DayInfo{
     dayInMonth: number;
     allTasks: any[] = [];
     importantTasks: any[] = [];
     hotTasks: any[] = [];
+    workload: number = 0;
 
     constructor(dayInMonth: number) {
         this.dayInMonth = dayInMonth;
+
+        this.workload = this.getRandomArbitrary(0,100);
+    }
+
+    private getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
     getTasksCount(): number{
@@ -18,5 +27,26 @@ export class DayInfo{
 
     getImportantTasksCount(): number{
         return this.importantTasks.length;
+    }
+
+    getDayWorkload(): string{
+        if(this.workload >= 0 && this.workload < 25){
+            return 'low';
+        } else
+        if(this.workload >= 25 && this.workload < 50){
+            return 'belowMedium';
+        } else 
+        if(this.workload >= 50 && this.workload < 75){
+            return 'aboveMedium';
+        } else
+        if(this.workload >= 75 && this.workload <= 100){
+            return 'high';
+        } else {            
+            return "incorrect value";
+        }
+    }
+
+    getWorkloadInPercents(): number{
+        return this.workload;
     }
 }
---
+++ eDiary.WebClient/src/shared/models/workload.enum.ts
@@ -0,0 +1,6 @@
+export enum Workload{
+    low = 1,
+    belowMedium = 2,
+    aboveMegium = 3,
+    high = 4
+}
