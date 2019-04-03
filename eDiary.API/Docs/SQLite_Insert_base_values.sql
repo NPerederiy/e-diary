07920038cace66e03d02cd41899a6d4f0c127727
NPerederiy <nick.perederiy@gmail.com>
2019-04-03 10:03:13 +0300

    Added SQL scripts for creating DB and inserting an initial data

---
+++ eDiary.API/Docs/SQLite_Insert_base_values.sql
@@ -0,0 +1,30 @@
+﻿INSERT INTO Statuses (Name) VALUES
+	("To do"),
+	("In progress"),
+	("To test"),
+	("Done"),
+	("Canceled")
+	
+INSERT INTO CardStatuses (Name) VALUES
+	("Hidden"),
+	("Completed"),
+	("Important"),
+	("Hot"),
+	("Deleted")
+	
+INSERT INTO Priorities (Name) VALUES
+	("Must have"),
+	("Should have"),
+	("Could have"),
+	("Would have")
+	
+INSERT INTO Difficulties (Name) VALUES
+	("Low"),
+	("Below average"),
+	("Above average"),
+	("High")
+
+INSERT INTO Languages (ShortCode) VALUES
+	("EN"),
+	("UK"),
+	("RU")
